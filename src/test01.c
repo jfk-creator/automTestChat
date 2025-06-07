7b7d61dd150b56c75b384b343caf79b4e8a3e583
Julian Kliebhan <hallo@jkliebhan.de>
2025-06-07 10:25:35 +0200

    buildClient()

--- src/test01.c
+++ src/test01.c
@@ -1,5 +1,6 @@
 #include "test01.h"
 
+#include <assert.h>
 #include <mqueue.h>
 #include <unistd.h>
 #define MAX_MSGS 10
@@ -21,46 +22,83 @@ mqd_t establishQueue(char* qName) {
         errorPrint("Mqueue not opened.");
         exit(EXIT_FAILURE);
     }
-    debugPrint("Mqueue openend");
+    debugPrint("Mqueue openend: %d", mq_handle);
     return mq_handle;
 }
 
-bool runTest_01() {
-    normalPrint("Starting Test01");
-    pthread_t threadID;
+void routine01(ClientArgs* clientArgs) {
+    char buffer[MAX_MSG_SIZE] = {0};
+
+    snprintf(buffer, MAX_MSG_SIZE, "CONNECT");
+    if (mq_send(clientArgs->mq, buffer, strlen(buffer) + 1, MSG_PRIO) == -1) {
+        errorPrint("mq_send failed");
+    }
+    debugPrint("Dispatcher: send message");
+
+    memset(&buffer, 0, sizeof(buffer));
+    unsigned int prio;
+    ssize_t bytes_read = mq_receive(clientArgs->mq, buffer, MAX_MSG_SIZE, &prio);
+    if (bytes_read == -1) {
+        errorPrint("receiv failed.");
+        exit(EXIT_FAILURE);
+    }
+    buffer[bytes_read] = '\0';
+    debugPrint("test01: %s received from %s", buffer, clientArgs->name);
+
+    sleep(1);
+
+    snprintf(buffer, MAX_MSG_SIZE, "QUIT");
+    if (mq_send(clientArgs->mq, buffer, strlen(buffer) + 1, MSG_PRIO) == -1) {
+        errorPrint("mq_send failed");
+    }
+    debugPrint("Dispatcher: send message");
+
+    sleep(3);
+}
+
+ClientArgs* buildClient(char* name) {
     ClientArgs* clientArgs = malloc(sizeof(ClientArgs));
     if (clientArgs == NULL) {
         errorPrint("clientArgs not allocated");
     }
-    strncpy(clientArgs->name, "Frederike", 32);
-    strncpy(clientArgs->qName, "/frederike", 32);
+    strncpy(clientArgs->name, name, sizeof(clientArgs->name));
+    snprintf(clientArgs->qName, sizeof(clientArgs->qName), "/%s", clientArgs->name);
+    clientArgs->qName[32] = '\0';
 
-    mqd_t mq_client = establishQueue(clientArgs->qName);
-    char buffer[MAX_MSG_SIZE];
-    int result = pthread_create(&threadID, NULL, client, (void*)clientArgs);
+    clientArgs->mq = establishQueue(clientArgs->qName);
+    int result = pthread_create(&clientArgs->threadId, NULL, client, (void*)clientArgs);
     if (result != 0) {
         errorPrint("Failed to create client thread");
         free(clientArgs);
-        return false;
+        exit(EXIT_FAILURE);
     } else {
-        debugPrint("Client thread created successfully with ID: %lu", (unsigned long)threadID);
+        debugPrint("Client thread created successfully with ID: %lu", (unsigned long)clientArgs->threadId);
     }
-    for (size_t i = 0; i < 10; i++) {
-        if (i == 5) {
-            snprintf(buffer, MAX_MSG_SIZE, "QUIT");
-            if (mq_send(mq_client, buffer, strlen(buffer) + 1, MSG_PRIO) == -1) {
-                errorPrint("mq_send failed");
-            }
-            debugPrint("Dispatcher: send message");
-        }
-        snprintf(buffer, MAX_MSG_SIZE, "This is what love feels like.");
-        if (mq_send(mq_client, buffer, strlen(buffer) + 1, MSG_PRIO) == -1) {
-            errorPrint("mq_send failed");
-        }
-        debugPrint("Dispatcher: send message");
-    }
+    assert(clientArgs != NULL);
+    return clientArgs;
+}
 
-    sleep(3);
+void deconstuctClient(ClientArgs* clientArgs) {
+    if (mq_close(clientArgs->mq) == -1) {
+        errorPrint("coudn't close mqeueu.");
+        exit(EXIT_FAILURE);
+    }
+    close(clientArgs->socket);
+    free(clientArgs);
+}
+
+bool runTest_01() {
+    normalPrint("Starting Test01");
+    ClientArgs* frederike = buildClient("Frederike");
+    ClientArgs* leon = buildClient("Leon");
+    ClientArgs* marina = buildClient("Marina");
+    routine01(frederike);
+    routine01(marina);
+    routine01(leon);
+
+    deconstuctClient(frederike);
+    deconstuctClient(marina);
+    deconstuctClient(leon);
 
     return true;
 }
