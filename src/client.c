410afbd0efbcb22469a9e6fb2c047bb722f1d1b5
Julian Kliebhan <hallo@jkliebhan.de>
2025-06-07 16:38:32 +0200

    added login cmd

--- src/client.c
+++ src/client.c
@@ -7,119 +7,146 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include "network.h"
+
 #define MAX_MSG_SIZE 512
 #define STANDARD_PORT 8111
 #define BUFFER_SIZE 1024
 
 static ClientArgs* clientArgs;
 
-typedef enum {
-    CONNECT,
-    QUIT,
-    SEND,
-    INVALID
-} COMMANDS;
+typedef enum { CONNECT, LOGIN, QUIT, SEND, INVALID } COMMANDS;
 
 COMMANDS parseCmd(char* mqMsg) {
-    debugPrint("%s received: %s", clientArgs->name, mqMsg);
+  debugPrint("%s received: %s", clientArgs->name, mqMsg);
 
-    if (strcmp("QUIT", mqMsg) == 0) {
-        return QUIT;
-    }
-    if (strcmp("CONNECT", mqMsg) == 0) {
-        return CONNECT;
-    }
-    return INVALID;
+  if (strcmp("QUIT", mqMsg) == 0) {
+    return QUIT;
+  }
+  if (strcmp("CONNECT", mqMsg) == 0) {
+    return CONNECT;
+  }
+  if (strcmp("LOGIN", mqMsg) == 0) {
+    return LOGIN;
+  }
+  return INVALID;
 }
 
-void handleQuit(mqd_t mq) {
-    debugPrint("Client: %s shutsdown.", clientArgs->name);
+void handleQuit() {
+  debugPrint("Client: %s shutsdown.", clientArgs->name);
 
-    if (mq_close(mq) == -1) {
-        errorPrint("coudn't close mqeueu.");
-        exit(EXIT_FAILURE);
-    }
-    pthread_exit(NULL);
+  if (mq_close(clientArgs->mq) == -1) {
+    errorPrint("coudn't close mqeueu.");
+    exit(EXIT_FAILURE);
+  }
+  pthread_exit(NULL);
 }
 
-void handleConnect(int16_t port, mqd_t mq) {
-    debugPrint("Client: %s connectiong to 127.0.0.1:%d", clientArgs->name, port);
-    struct sockaddr_in server_addr;
-    // char buffer[BUFFER_SIZE] = {0};
+void handleConnect(int16_t port) {
+  debugPrint("Client: %s connectiong to 127.0.0.1:%d", clientArgs->name, port);
+  struct sockaddr_in server_addr;
+  // char buffer[BUFFER_SIZE] = {0};
 
-    clientArgs->socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientArgs->socket == -1) {
-        errorPrint("Error creating socket");
-        exit(EXIT_FAILURE);
-    }
+  clientArgs->socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (clientArgs->socket == -1) {
+    errorPrint("Error creating socket");
+    exit(EXIT_FAILURE);
+  }
 
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(STANDARD_PORT);
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(STANDARD_PORT);
 
-    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
-        errorPrint("Invalid address/ Address not supported");
-        close(clientArgs->socket);
-        exit(EXIT_FAILURE);
-    }
+  if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
+    errorPrint("Invalid address/ Address not supported");
+    close(clientArgs->socket);
+    exit(EXIT_FAILURE);
+  }
 
-    if (connect(clientArgs->socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        errorPrint("Error connecting to server");
-        close(clientArgs->socket);
-        exit(EXIT_FAILURE);
-    }
-    normalPrint("Successfully connected to the server.\n");
+  if (connect(clientArgs->socket, (struct sockaddr*)&server_addr,
+              sizeof(server_addr)) == -1) {
+    errorPrint("Error connecting to server");
+    close(clientArgs->socket);
+    exit(EXIT_FAILURE);
+  }
+  normalPrint("Successfully connected to the server.\n");
 
-    char mq_buffer[MAX_MSG_SIZE];
-    snprintf(mq_buffer, MAX_MSG_SIZE, "OK");
-    if (mq_send(mq, mq_buffer, strlen(mq_buffer) + 1, 1) == -1) {
-        errorPrint("mq_send failed");
-    }
-    debugPrint("Dispatcher: send message");
+  char mq_buffer[MAX_MSG_SIZE];
+  snprintf(mq_buffer, MAX_MSG_SIZE, "OK");
+  if (mq_send(clientArgs->mq, mq_buffer, strlen(mq_buffer) + 1, 1) == -1) {
+    errorPrint("mq_send failed");
+  }
+  sleep(2);
+}
+
+void handleLogin() {
+  // build paket
+  LGQ loginRequest = {.magic = htons(MAGIC_NUMBER), .version = VERSION};
+  strncpy(loginRequest.name, clientArgs->name, sizeof(loginRequest.name) - 1);
+  paket_t paket = {.type = 0,
+                   .length = htons(sizeof(loginRequest)),
+                   .logingRequest = loginRequest};
+
+  // send it
+  ssize_t bytes_sent =
+      send(clientArgs->socket, (const char*)&paket, sizeof(paket), 0);
+  if (bytes_sent == -1) {
+    errorPrint("Error sending login request");
+    close(clientArgs->socket);
+    exit(EXIT_FAILURE);
+  } else if (bytes_sent != sizeof(paket)) {
+    errorPrint("Warning: Sent only %zd out of %zu bytes for login request.\n",
+               bytes_sent, sizeof(paket));
+  } else {
+    normalPrint("Login request sent successfully (%zd bytes).", bytes_sent);
+  }
 }
 
 void* client(void* arg) {
-    clientArgs = (ClientArgs*)arg;
-    debugPrint("starting client: %s", clientArgs->name);
-    // establish mqueue
-    mqd_t mq = mq_open(clientArgs->qName, O_RDWR);
-    if (mq == (mqd_t)-1) {
-        errorPrint("mq not openend.");
-        exit(EXIT_FAILURE);
+  clientArgs = (ClientArgs*)arg;
+  debugPrint("starting client: %s", clientArgs->name);
+  // establish mqueue
+  mqd_t mq = mq_open(clientArgs->qName, O_RDWR);
+  if (mq == (mqd_t)-1) {
+    errorPrint("client-%s: mq not openend.", clientArgs->name);
+    exit(EXIT_FAILURE);
+  }
+  debugPrint("client %s opened Mqeueu", clientArgs->name);
+
+  while (1) {
+    unsigned int prio;
+    char buffer[MAX_MSG_SIZE];
+    ssize_t bytes_read = mq_receive(mq, buffer, MAX_MSG_SIZE, &prio);
+    if (bytes_read == -1) {
+      errorPrint("receiv failed.");
+      exit(EXIT_FAILURE);
     }
-    debugPrint("client %s opened Mqeueu", clientArgs->name);
+    buffer[bytes_read] = '\0';
 
-    while (1) {
-        unsigned int prio;
-        char buffer[MAX_MSG_SIZE];
-        ssize_t bytes_read = mq_receive(mq, buffer, MAX_MSG_SIZE, &prio);
-        if (bytes_read == -1) {
-            errorPrint("receiv failed.");
-            exit(EXIT_FAILURE);
-        }
-        buffer[bytes_read] = '\0';
+    switch (parseCmd(buffer)) {
+      case CONNECT:
+        handleConnect(STANDARD_PORT);
+        break;
+      case LOGIN:
+        handleLogin();
+        break;
+      case QUIT:
 
-        switch (parseCmd(buffer)) {
-            case CONNECT:
-                handleConnect(STANDARD_PORT, mq);
-                break;
-            case QUIT:
+        handleQuit();
+        break;
+      case INVALID:
+        errorPrint("Mq received an invalid command.");
+        break;
 
-                handleQuit(mq);
-                break;
-            case INVALID:
-                errorPrint("Mq received an invalid command.");
-                break;
-
-            default:
-                errorPrint("Hier hab ich mein Auto nicht geparkt.");
-                break;
-        }
+      default:
+        errorPrint("Hier hab ich mein Auto nicht geparkt.");
+        break;
     }
-    if (mq_close(mq) == -1) {
-        errorPrint("coudn't close mqeueu.");
-        exit(EXIT_FAILURE);
-    }
+  }
+  if (mq_close(mq) == -1) {
+    errorPrint("coudn't close mqeueu.");
+    exit(EXIT_FAILURE);
+  }
 
-    pthread_exit(NULL);
+  pthread_exit(NULL);
 }
--- src/test01.c
+++ src/test01.c
@@ -8,97 +8,105 @@
 #define MSG_PRIO 1
 
 mqd_t establishQueue(char* qName) {
-    mqd_t mq_handle;
-    struct mq_attr attr;
+  mqd_t mq_handle;
+  struct mq_attr attr;
 
-    attr.mq_flags = 0;
-    attr.mq_maxmsg = MAX_MSGS;
-    attr.mq_msgsize = MAX_MSG_SIZE;
-    attr.mq_curmsgs = 0;
+  attr.mq_flags = 0;
+  attr.mq_maxmsg = MAX_MSGS;
+  attr.mq_msgsize = MAX_MSG_SIZE;
+  attr.mq_curmsgs = 0;
 
-    mq_handle = mq_open(qName, O_CREAT | O_RDWR, 0644, &attr);
+  mq_handle = mq_open(qName, O_CREAT | O_RDWR, 0644, &attr);
 
-    if (mq_handle == (mqd_t)-1) {
-        errorPrint("Mqueue not opened.");
-        exit(EXIT_FAILURE);
-    }
-    debugPrint("Mqueue openend: %d", mq_handle);
-    return mq_handle;
+  if (mq_handle == (mqd_t)-1) {
+    errorPrint("Mqueue not opened.");
+    exit(EXIT_FAILURE);
+  }
+  debugPrint("Mqueue openend: %d", mq_handle);
+  return mq_handle;
 }
 
 void routine01(ClientArgs* clientArgs) {
-    char buffer[MAX_MSG_SIZE] = {0};
+  char buffer[MAX_MSG_SIZE] = {0};
 
-    snprintf(buffer, MAX_MSG_SIZE, "CONNECT");
-    if (mq_send(clientArgs->mq, buffer, strlen(buffer) + 1, MSG_PRIO) == -1) {
-        errorPrint("mq_send failed");
-    }
-    debugPrint("Dispatcher: send message");
+  snprintf(buffer, MAX_MSG_SIZE, "CONNECT");
+  if (mq_send(clientArgs->mq, buffer, strlen(buffer) + 1, MSG_PRIO) == -1) {
+    errorPrint("mq_send failed");
+  }
+  debugPrint("Dispatcher: send message");
 
-    memset(&buffer, 0, sizeof(buffer));
-    unsigned int prio;
-    ssize_t bytes_read = mq_receive(clientArgs->mq, buffer, MAX_MSG_SIZE, &prio);
-    if (bytes_read == -1) {
-        errorPrint("receiv failed.");
-        exit(EXIT_FAILURE);
-    }
-    buffer[bytes_read] = '\0';
-    debugPrint("test01: %s received from %s", buffer, clientArgs->name);
+  sleep(1);
 
-    sleep(1);
+  memset(&buffer, 0, sizeof(buffer));
+  unsigned int prio;
+  ssize_t bytes_read = mq_receive(clientArgs->mq, buffer, MAX_MSG_SIZE, &prio);
+  if (bytes_read == -1) {
+    errorPrint("receiv failed.");
+    exit(EXIT_FAILURE);
+  }
+  buffer[bytes_read] = '\0';
+  debugPrint("test01: %s received from %s", buffer, clientArgs->name);
 
-    snprintf(buffer, MAX_MSG_SIZE, "QUIT");
-    if (mq_send(clientArgs->mq, buffer, strlen(buffer) + 1, MSG_PRIO) == -1) {
-        errorPrint("mq_send failed");
-    }
-    debugPrint("Dispatcher: send message");
+  sleep(1);
 
-    sleep(3);
+  snprintf(buffer, MAX_MSG_SIZE, "LOGIN");
+  if (mq_send(clientArgs->mq, buffer, strlen(buffer) + 1, MSG_PRIO) == -1) {
+    errorPrint("mq_send failed");
+  }
+  sleep(1);
+
+  debugPrint("Dispatcher: send message");
+
+  snprintf(buffer, MAX_MSG_SIZE, "QUIT");
+  if (mq_send(clientArgs->mq, buffer, strlen(buffer) + 1, MSG_PRIO) == -1) {
+    errorPrint("mq_send failed");
+  }
+  debugPrint("Dispatcher: send message");
+
+  sleep(3);
 }
 
 ClientArgs* buildClient(char* name) {
-    ClientArgs* clientArgs = malloc(sizeof(ClientArgs));
-    if (clientArgs == NULL) {
-        errorPrint("clientArgs not allocated");
-    }
-    strncpy(clientArgs->name, name, sizeof(clientArgs->name));
-    snprintf(clientArgs->qName, sizeof(clientArgs->qName), "/%s", clientArgs->name);
-    clientArgs->qName[32] = '\0';
+  ClientArgs* clientArgs = malloc(sizeof(ClientArgs));
+  if (clientArgs == NULL) {
+    errorPrint("clientArgs not allocated");
+  }
+  strncpy(clientArgs->name, name, sizeof(clientArgs->name));
+  snprintf(clientArgs->qName, sizeof(clientArgs->qName), "/%s",
+           clientArgs->name);
+  clientArgs->qName[32] = '\0';
 
-    clientArgs->mq = establishQueue(clientArgs->qName);
-    int result = pthread_create(&clientArgs->threadId, NULL, client, (void*)clientArgs);
-    if (result != 0) {
-        errorPrint("Failed to create client thread");
-        free(clientArgs);
-        exit(EXIT_FAILURE);
-    } else {
-        debugPrint("Client thread created successfully with ID: %lu", (unsigned long)clientArgs->threadId);
-    }
-    assert(clientArgs != NULL);
-    return clientArgs;
+  clientArgs->mq = establishQueue(clientArgs->qName);
+  int result =
+      pthread_create(&clientArgs->threadId, NULL, client, (void*)clientArgs);
+  if (result != 0) {
+    errorPrint("Failed to create client thread");
+    free(clientArgs);
+    exit(EXIT_FAILURE);
+  } else {
+    debugPrint("Client thread created successfully with ID: %lu",
+               (unsigned long)clientArgs->threadId);
+  }
+  assert(clientArgs != NULL);
+  return clientArgs;
 }
 
 void deconstuctClient(ClientArgs* clientArgs) {
-    if (mq_close(clientArgs->mq) == -1) {
-        errorPrint("coudn't close mqeueu.");
-        exit(EXIT_FAILURE);
-    }
-    close(clientArgs->socket);
-    free(clientArgs);
+  if (mq_close(clientArgs->mq) == -1) {
+    errorPrint("coudn't close mqeueu.");
+    exit(EXIT_FAILURE);
+  }
+  close(clientArgs->socket);
+  free(clientArgs);
 }
 
 bool runTest_01() {
-    normalPrint("Starting Test01");
-    ClientArgs* frederike = buildClient("Frederike");
-    ClientArgs* leon = buildClient("Leon");
-    ClientArgs* marina = buildClient("Marina");
-    routine01(frederike);
-    routine01(marina);
-    routine01(leon);
+  normalPrint("Starting Test01");
+  ClientArgs* frederike = buildClient("Frederike");
 
-    deconstuctClient(frederike);
-    deconstuctClient(marina);
-    deconstuctClient(leon);
+  routine01(frederike);
 
-    return true;
+  deconstuctClient(frederike);
+
+  return true;
 }
