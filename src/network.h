31592f5875aa689b6dfc67430fc1aa890c6d3af4
Julian Kliebhan <hallo@jkliebhan.de>
2025-06-07 21:26:16 +0200

    added LRE and a seperate Head struct

--- src/network.h
+++ src/network.h
@@ -1,5 +1,12 @@
 
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <errno.h>
 #include <stdint.h>
+#include <unistd.h>
+
+#include "utils.h"
 
 #define MAGIC_NUMBER 0x0badf00d
 #define VERSION 0
@@ -11,7 +18,26 @@ typedef struct {
 } __attribute__((packed)) LGQ;
 
 typedef struct {
+  uint32_t magic;
+  uint8_t code;
+  char sName[31];
+} __attribute__((packed)) LRE;
+
+union Body {
+  LGQ loginRequest;
+  LRE loginResponse;
+};
+
+typedef struct {
   uint8_t type;
   uint16_t length;
-  LGQ logingRequest;
+} __attribute__((packed)) Head;
+
+typedef struct {
+  uint8_t type;
+  uint16_t length;
+  union Body body;
 } __attribute__((packed)) paket_t;
+
+ssize_t recv_all(int socket_fd, void *buffer, size_t len);
+#endif
